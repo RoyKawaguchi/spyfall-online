9e47561271b79c141ecc6f7f4079f16c5494e796
RoyKawaguchi <roygreenhills@gmail.com>
2026-02-09 11:36:35 +0900

    Fix hard coded PORT

--- server.js
+++ server.js
@@ -209,6 +209,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Spyfall Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Spyfall Server running");
 });
